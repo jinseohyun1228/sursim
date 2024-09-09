1a5602e918ff2d99b7940c16827e430e099d200b
JinSeoHyun <15roa1121@gmail.com>
2024-09-10 01:38:30 +0900

    feat: 헬시엔드포인트 추가

--- src/main/java/com/pnu/sursim/global/HealthController.java
+++ src/main/java/com/pnu/sursim/global/HealthController.java
@@ -1,15 +1,16 @@
 package com.pnu.sursim.global;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-public class TestController {
+public class HealthController {
 
     @GetMapping("/health")
-    public String healthTest() {
-        return "health";
+    public ResponseEntity<?> healthTest() {
+        return ResponseEntity.ok("It's working very well~!");
     }
 }
