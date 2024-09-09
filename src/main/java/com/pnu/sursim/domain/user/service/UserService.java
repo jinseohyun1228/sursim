ce175364b98db2961f703dc155227bb8beb308e8
JinSeoHyun <15roa1121@gmail.com>
2024-09-10 01:40:23 +0900

    feat: 카카오 로그인과 유저 관련 기능 분리

--- src/main/java/com/pnu/sursim/domain/user/service/UserService.java
+++ src/main/java/com/pnu/sursim/domain/user/service/UserService.java
@@ -1,9 +1,10 @@
 package com.pnu.sursim.domain.user.service;
 
-import com.pnu.sursim.domain.user.dto.*;
+import com.pnu.sursim.domain.user.dto.AuthUser;
+import com.pnu.sursim.domain.user.dto.ProfileRequest;
+import com.pnu.sursim.domain.user.dto.UserVo;
 import com.pnu.sursim.domain.user.entity.User;
 import com.pnu.sursim.domain.user.repository.UserRepository;
-import com.pnu.sursim.global.auth.jwt.JWTUtil;
 import com.pnu.sursim.global.exception.CustomException;
 import com.pnu.sursim.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
@@ -14,23 +15,6 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
-    private final JWTUtil jwtUtil;
-
-    public String joinUser(JoinRequest joinRequest) {
-        if (userRepository.existsByEmail(joinRequest.email())) {
-            throw new CustomException(ErrorCode.EMAIL_EXISTS);
-        }
-
-        User savedUser = userRepository.save(new User(joinRequest));
-        return jwtUtil.createToken(new UserVo(savedUser));
-
-    }
-
-    public String loginUser(LoginRequest loginRequest) {
-        User user = userRepository.findByEmailAndPassword(loginRequest.email(), loginRequest.password())
-                .orElseThrow(() -> new CustomException(ErrorCode.LOGIN_ERROR));
-        return jwtUtil.createToken(new UserVo(user));
-    }
 
     public UserVo getUserInfo(AuthUser authUser) {
         User user = userRepository.findByEmail(authUser.getEmail())
