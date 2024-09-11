86c0ea0871d0a35ad5ba6f0a0d6e57b7e74adcc8
JinSeoHyun <15roa1121@gmail.com>
2024-09-12 00:38:30 +0900

    feat : 한글 값으로 Json 객체 만들기

--- src/main/java/com/pnu/sursim/domain/user/dto/JoinRequest.java
+++ src/main/java/com/pnu/sursim/domain/user/dto/JoinRequest.java
@@ -17,35 +17,35 @@ public record JoinRequest(String name,
                           Gender gender,
                           Region region) {
 
-    public JoinRequest {
-        // 이름 검증
-        if (name == null || name.isBlank()) {
-            throw new CustomException(ErrorCode.EMPTY_VALUE_NOT_POSSIBLE);
-        }
-
-        // 이메일 검증
-        if (email == null || !email.contains("@")) {
-            throw new CustomException(ErrorCode.EMPTY_VALUE_NOT_POSSIBLE);
-        }
-
-        // 비밀번호 검증
-        if (password == null || password.length() < 8) {
-            throw new CustomException(ErrorCode.EMPTY_VALUE_NOT_POSSIBLE);
-        }
-
-        // 생년월일 검증
-        if (birthDate == null) {
-            throw new CustomException(ErrorCode.EMPTY_VALUE_NOT_POSSIBLE);
-        }
-
-        // 성별 검증
-        if (gender == null) {
-            throw new CustomException(ErrorCode.EMPTY_VALUE_NOT_POSSIBLE);
-        }
-
-        // 지역 검증
-        if (region == null) {
-            throw new CustomException(ErrorCode.EMPTY_VALUE_NOT_POSSIBLE);
-        }
-    }
+//    public JoinRequest {
+//        // 이름 검증
+//        if (name == null || name.isBlank()) {
+//            throw new CustomException(ErrorCode.EMPTY_VALUE_NOT_POSSIBLE);
+//        }
+//
+//        // 이메일 검증
+//        if (email == null || !email.contains("@")) {
+//            throw new CustomException(ErrorCode.EMPTY_VALUE_NOT_POSSIBLE);
+//        }
+//
+//        // 비밀번호 검증
+//        if (password == null || password.length() < 8) {
+//            throw new CustomException(ErrorCode.EMPTY_VALUE_NOT_POSSIBLE);
+//        }
+//
+//        // 생년월일 검증
+//        if (birthDate == null) {
+//            throw new CustomException(ErrorCode.EMPTY_VALUE_NOT_POSSIBLE);
+//        }
+//
+//        // 성별 검증
+//        if (gender == null) {
+//            throw new CustomException(ErrorCode.EMPTY_VALUE_NOT_POSSIBLE);
+//        }
+//
+//        // 지역 검증
+//        if (region == null) {
+//            throw new CustomException(ErrorCode.EMPTY_VALUE_NOT_POSSIBLE);
+//        }
+//    }
 }
