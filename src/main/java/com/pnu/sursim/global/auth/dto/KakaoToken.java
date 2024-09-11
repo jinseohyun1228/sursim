5592a5b2c9c3368939fc3a8619f03f98b8dfb46d
JinSeoHyun <15roa1121@gmail.com>
2024-09-11 23:25:58 +0900

    remove : 필요없는 코드 삭제

--- src/main/java/com/pnu/sursim/global/auth/dto/KakaoToken.java
+++ src/main/java/com/pnu/sursim/global/auth/dto/KakaoToken.java
@@ -3,7 +3,6 @@ package com.pnu.sursim.global.auth.dto;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
-import java.time.LocalDateTime;
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record KakaoToken(String accessToken) {
