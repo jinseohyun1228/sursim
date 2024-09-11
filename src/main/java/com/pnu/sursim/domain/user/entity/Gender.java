cb71d123382c41737c5a129236a1fdbade4fa503
JinSeoHyun <15roa1121@gmail.com>
2024-09-12 00:38:38 +0900

    feat : 한글 값으로 Json 객체 만들기

--- src/main/java/com/pnu/sursim/domain/user/entity/Gender.java
+++ src/main/java/com/pnu/sursim/domain/user/entity/Gender.java
@@ -1,5 +1,8 @@
 package com.pnu.sursim.domain.user.entity;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum Gender{
     FEMALE("여성"),
     MALE("남성"),;
@@ -11,6 +14,24 @@ public enum Gender{
     }
 
     public String getKoreanName() {
+        return koreanName;
+    }
+
+
+    // JSON에서 한글 이름으로 매핑
+    @JsonCreator
+    public static Gender fromKoreanName(String koreanName) {
+        for (Gender gender : Gender.values()) {
+            if (gender.koreanName.equals(koreanName)) {
+                return gender;
+            }
+        }
+        throw new IllegalArgumentException("Invalid gender: " + koreanName);
+    }
+
+    // JSON 출력 시 한글 이름으로 출력
+    @JsonValue
+    public String toKoreanName() {
         return koreanName;
     }
 }
