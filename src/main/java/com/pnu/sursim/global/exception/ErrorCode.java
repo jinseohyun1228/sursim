dc8818c46ee1498a486a47ad8cb6cd9a4e64d1a6
JinSeoHyun <15roa1121@gmail.com>
2024-09-11 23:09:53 +0900

    feat : 로그인과정중 에러 코드 추가

--- src/main/java/com/pnu/sursim/global/exception/ErrorCode.java
+++ src/main/java/com/pnu/sursim/global/exception/ErrorCode.java
@@ -7,9 +7,11 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     //회원가입
     EMAIL_EXISTS(HttpStatus.BAD_REQUEST,"This email already exists."),
+    EMPTY_VALUE_NOT_POSSIBLE(HttpStatus.BAD_REQUEST,"Contains an invalid value."),
 
     //로그인
     LOGIN_ERROR(HttpStatus.BAD_REQUEST, "Invalid email and password."),
+    KAKAO_LOGIN_ERROR_NO_USER(HttpStatus.BAD_REQUEST, "User information could not be received from Kakao. The token may be incorrect."),
 
     //토큰
     TOKEN_EXPIRED(HttpStatus.BAD_REQUEST,"Your session has expired."),
