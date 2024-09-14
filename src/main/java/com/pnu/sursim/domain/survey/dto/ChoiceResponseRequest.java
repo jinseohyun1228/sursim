56ae7653d95d89dfecae402f3443b0d3efb75dda
JinSeoHyun <15roa1121@gmail.com>
2024-09-14 12:47:33 +0900

    feat : 파일이름변경과 필수옵션추가

--- src/main/java/com/pnu/sursim/domain/survey/dto/ChoiceResponseRequest.java
+++
@@ -1,5 +0,0 @@
-package com.pnu.sursim.domain.survey.dto;
-
-public record ChoiceResponseRequest(int number,
-                                    String text) {
-}
---
+++ src/main/java/com/pnu/sursim/domain/survey/entity/ChoiceResponse.java
@@ -0,0 +1,4 @@
+package com.pnu.sursim.domain.survey.entity;
+
+public class ChoiceResponse {
+}
