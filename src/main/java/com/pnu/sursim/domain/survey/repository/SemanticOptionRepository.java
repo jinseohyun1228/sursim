9da89a945d9584457bf8a94f28e1e8f18a7d2183
JinSeoHyun <15roa1121@gmail.com>
2024-09-15 20:42:03 +0900

    feat: 의미판별항목 엔티티와 리파지토리

---
+++ src/main/java/com/pnu/sursim/domain/survey/repository/SemanticOptionRepository.java
@@ -0,0 +1,7 @@
+package com.pnu.sursim.domain.survey.repository;
+
+import com.pnu.sursim.domain.survey.entity.SemanticOption;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SemanticOptionRepository extends JpaRepository<SemanticOption, Long> {
+}
