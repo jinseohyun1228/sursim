0f43d0517913286e531f87bc5215e17233819d31
JinSeoHyun <15roa1121@gmail.com>
2024-09-15 20:39:14 +0900

    feat: Survey에 필요한 ENUM(문항타입)추가

--- src/main/java/com/pnu/sursim/domain/survey/entity/QuestionType.java
+++ src/main/java/com/pnu/sursim/domain/survey/entity/QuestionType.java
@@ -1,19 +1,26 @@
 package com.pnu.sursim.domain.survey.entity;
 
 public enum QuestionType {
-    CHECK_CHOICE("체크박스"),
-    SEMANTIC_RATINGS("의미분별척도"),
-    LIKERT_SCORES("리커트척도"),
-    MULTIPLE_CHOICE("객관식"),
-    NUMERIC_RESPONSE("숫자응답"),
-    PHONE_NUMBER("전화번호");
+    CHECK_CHOICE("체크박스", 1), // 1분
+    SEMANTIC_RATINGS("의미분별척도", 2), // 2분
+    LIKERT_SCORES("리커트척도", 2), // 2분
+    MULTIPLE_CHOICE("객관식", 1), // 1분
+    NUMERIC_RESPONSE("숫자응답", 1), // 1분
+    PHONE_NUMBER("전화번호", 1), // 1분
+    SUBJECTIVE("주관식", 3), // 3분
+    DESCRIPTIVE("서술형", 5); // 5분
 
     private String description;
+    private final int timeTaken;
 
-    QuestionType(String description) {
+    QuestionType(String description, int minutes) {
         this.description = description;
+        this.timeTaken = minutes;
     }
 
+    public int getTimeTaken() {
+        return timeTaken;
+    }
     public String getDescription() {
         return description;
     }
