f260a9b6e16c847f04dfc0c4c4c0db097e20dae7
JinSeoHyun <15roa1121@gmail.com>
2024-09-15 20:44:19 +0900

    fix: 객체 변수명 변경으로 인한 코드 수정

--- src/main/java/com/pnu/sursim/domain/survey/util/SurveyRequiredTimeCalculator.java
+++ src/main/java/com/pnu/sursim/domain/survey/util/SurveyRequiredTimeCalculator.java
@@ -5,7 +5,7 @@ import com.pnu.sursim.domain.survey.dto.SurveyRequest;
 public class SurveyRequiredTimeCalculator {
 
     public static int calculateRequiredTime(SurveyRequest surveyRequest) {
-        int timeRequired = surveyRequest.questionRequestList()
+        int timeRequired = surveyRequest.questionList()
                 .stream() // 문항 리스트를 스트림으로 변환
                 .mapToInt(questionRequest -> questionRequest.questionType().getTimeTaken()) // 각 문항의 소요 시간을 정수로 매핑
                 .sum();
