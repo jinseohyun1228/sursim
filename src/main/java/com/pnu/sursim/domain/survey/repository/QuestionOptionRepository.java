c797ed603f6945ae57291f0c912cc75dfc716dab
JinSeoHyun <15roa1121@gmail.com>
2024-09-16 19:54:12 +0900

    feat: SemanticOptionRepository에 문항id값으로 찾는 로직 추가

--- src/main/java/com/pnu/sursim/domain/survey/repository/QuestionOptionRepository.java
+++ src/main/java/com/pnu/sursim/domain/survey/repository/QuestionOptionRepository.java
@@ -3,5 +3,9 @@ package com.pnu.sursim.domain.survey.repository;
 import com.pnu.sursim.domain.survey.entity.QuestionOption;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface QuestionOptionRepository extends JpaRepository<QuestionOption, Long> {
+
+    List<QuestionOption> findAllByQuestionId(Long id);
 }
--- src/main/java/com/pnu/sursim/domain/survey/repository/SemanticOptionRepository.java
+++ src/main/java/com/pnu/sursim/domain/survey/repository/SemanticOptionRepository.java
@@ -3,5 +3,8 @@ package com.pnu.sursim.domain.survey.repository;
 import com.pnu.sursim.domain.survey.entity.SemanticOption;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SemanticOptionRepository extends JpaRepository<SemanticOption, Long> {
+    Optional<SemanticOption> findByQuestionId(Long id);
 }
