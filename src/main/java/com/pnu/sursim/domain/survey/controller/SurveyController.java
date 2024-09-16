d4b5ddc93231a45cda0bc94abfd63008f10b4558
JinSeoHyun <15roa1121@gmail.com>
2024-09-16 19:57:29 +0900

    feat: 모든 설문조사 가져오는 Url 추가

--- src/main/java/com/pnu/sursim/domain/survey/controller/SurveyController.java
+++ src/main/java/com/pnu/sursim/domain/survey/controller/SurveyController.java
@@ -1,15 +1,17 @@
 package com.pnu.sursim.domain.survey.controller;
 
 import com.pnu.sursim.domain.survey.dto.SurveyRequest;
+import com.pnu.sursim.domain.survey.dto.SurveyResponse;
 import com.pnu.sursim.domain.survey.service.SurveyService;
 import com.pnu.sursim.domain.user.dto.AuthUser;
 import com.pnu.sursim.global.auth.resolver.SessionUser;
 import com.pnu.sursim.global.response.CustomResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -18,11 +20,21 @@ public class SurveyController {
 
     private final SurveyService surveyService;
 
-    @PostMapping("/survey")
+    @PostMapping("/surveys")
     public CustomResponse createSurvey(@SessionUser AuthUser authUser, @RequestBody SurveyRequest surveyRequest) {
         surveyService.createSurvey(authUser.getEmail(),surveyRequest);
         return CustomResponse.success("Survey created successfully");
     }
+    @GetMapping("/surveys/all")
+    public CustomResponse getSurveysForAll(@SessionUser AuthUser authUser,Pageable pageable) {
+        Page<SurveyResponse> surveyResponsePage = surveyService.getSurveysForAll(authUser.getEmail(),pageable);
+        return CustomResponse.success(surveyResponsePage);
+    }
 
+    @GetMapping("/surveys")
+    public CustomResponse getSurveysForUser(@SessionUser AuthUser authUser, Pageable pageable) {
+        Page<SurveyResponse> surveyResponsePage = surveyService.getSurveysForUser(authUser.getEmail(),pageable);
+        return CustomResponse.success(surveyResponsePage);
+    }
 
 }
