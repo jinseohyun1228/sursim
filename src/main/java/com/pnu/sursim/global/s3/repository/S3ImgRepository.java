05c66c8eb000931ce51a6ae4cd2f31e3a24b3e08
JinSeoHyun <15roa1121@gmail.com>
2024-09-17 13:39:14 +0900

    feat: s3에 올린 이미지 저장하는 엔티티와 저장소 구현

---
+++ src/main/java/com/pnu/sursim/global/s3/repository/S3ImgRepository.java
@@ -0,0 +1,7 @@
+package com.pnu.sursim.global.s3.repository;
+
+import com.pnu.sursim.global.s3.entity.S3Img;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface S3ImgRepository extends JpaRepository<S3Img,Long> {
+}
