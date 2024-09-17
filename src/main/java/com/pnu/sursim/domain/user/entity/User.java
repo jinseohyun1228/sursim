c9464688ff953d1dfe63ef6a2c1fdb9d241650a9
JinSeoHyun <15roa1121@gmail.com>
2024-09-17 22:00:39 +0900

    feat: 유저엔티티에 리워드 필드와 equals 오버라이딩

--- src/main/java/com/pnu/sursim/domain/user/entity/User.java
+++ src/main/java/com/pnu/sursim/domain/user/entity/User.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 @Getter
 @Entity
@@ -95,5 +96,18 @@ public class User {
         this.gender = gender;
         this.region = region;
         this.userInfoStatus = UserInfoStatus.fromUser(this);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && Objects.equals(email, user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, email);
     }
 }
