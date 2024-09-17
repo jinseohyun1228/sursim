cf5cdd27f1e4ad8e54f4bad716fa7555e05a9bb7
JinSeoHyun <15roa1121@gmail.com>
2024-09-17 21:58:33 +0900

    feat: 리워드 엔티티와 설문조사 엔티티에 리워드 타입여부 추가

---
+++ src/main/java/com/pnu/sursim/domain/survey/entity/RewardStatus.java
@@ -0,0 +1,6 @@
+package com.pnu.sursim.domain.survey.entity;
+
+public enum RewardStatus {
+    HAS_REWARD,
+    NO_REWARD
+}
---
+++ src/main/java/com/pnu/sursim/domain/survey/repository/RewardRepository.java
@@ -0,0 +1,8 @@
+package com.pnu.sursim.domain.survey.repository;
+
+import com.pnu.sursim.domain.survey.entity.Reward;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RewardRepository extends JpaRepository<Reward,Long> {
+
+}
