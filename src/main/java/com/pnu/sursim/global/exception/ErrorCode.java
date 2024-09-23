4474505e71d61845cde3e7a4fa255398690e655c
JinSeoHyun <15roa1121@gmail.com>
2024-09-23 21:24:53 +0900

    feat : 문항이없는 경우에 대한 에러 코드 추가

--- src/main/java/com/pnu/sursim/global/exception/ErrorCode.java
+++ src/main/java/com/pnu/sursim/global/exception/ErrorCode.java
@@ -27,13 +27,14 @@ public enum ErrorCode {
     SURVEY_DOES_NOT_EXIST(HttpStatus.BAD_REQUEST, "There is no survey matching that ID value."),
     INCORRECT_CHOICE_QUESTION(HttpStatus.BAD_REQUEST, "An error occurred in the CHOICE_QUESTION. This may be a wrong survey. Please contact the developer."),
     INCORRECT_SEMANTIC_QUESTIONS(HttpStatus.BAD_REQUEST, "An error occurred in the SEMANTIC_QUESTIONS. This may be a wrong survey. Please contact the developer."),
+    INCORRECT_QUESTION(HttpStatus.BAD_REQUEST,"There was a problem with the survey questions. Please contact the administrator."),
 
     //이미지업로드 중 오류
     ERROR_UPLOADING_IMAGE(HttpStatus.BAD_REQUEST, "An error occurred while uploading s3. Please contact the developer."),
 
     //설문조사에 응답 과정 중 에러
-    SURVEY_CONSENT_REQUIRED(HttpStatus.BAD_REQUEST,"Surveys require consent.")
-    ;
+    SURVEY_CONSENT_REQUIRED(HttpStatus.BAD_REQUEST,"Surveys require consent."),
+    QUESTION_NOT_FOUND(HttpStatus.BAD_REQUEST,"The question cannot be found.");
 
     private final HttpStatus httpStatus;
     private final String errorMessage;
