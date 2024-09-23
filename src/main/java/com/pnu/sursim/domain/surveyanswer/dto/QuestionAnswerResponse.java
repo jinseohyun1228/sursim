423d822c3f6f7b930cc642861498420a8cdc6c12
JinSeoHyun <15roa1121@gmail.com>
2024-09-24 02:10:25 +0900

    feat: 사용자에게 응답받는 객체 구현

---
+++ src/main/java/com/pnu/sursim/domain/surveyanswer/dto/QuestionAnswerResponse.java
@@ -0,0 +1,24 @@
+package com.pnu.sursim.domain.surveyanswer.dto;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.pnu.sursim.domain.survey.entity.QuestionType;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+public class QuestionAnswerResponse {
+    private Long id;                            //문항 아이디
+    private QuestionType questionType;          //문항 타입
+    private Integer optionNumber;               //문항 번호
+    private String text;                        //문항 텍스트
+    private String phoneNumber;                 //전화번호
+    private List<Integer> checkList;            //체크리스트
+}
---
+++ src/main/java/com/pnu/sursim/domain/surveyanswer/dto/SurveyAnswerRequest.java
@@ -0,0 +1,21 @@
+package com.pnu.sursim.domain.surveyanswer.dto;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.pnu.sursim.domain.survey.entity.ConsentStatus;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+public class SurveyAnswerRequest {
+    private List<QuestionAnswerResponse> questionAnswerList;
+    private ConsentStatus consentStatus;
+
+}
