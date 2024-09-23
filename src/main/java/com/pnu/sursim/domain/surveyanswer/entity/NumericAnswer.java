8f93b6bad1bddafb31e8d849362f6b360fde604a
JinSeoHyun <15roa1121@gmail.com>
2024-09-24 02:08:52 +0900

    feat: 숫자 응답 엔티티 구현

---
+++ src/main/java/com/pnu/sursim/domain/surveyanswer/entity/NumericAnswer.java
@@ -0,0 +1,31 @@
+package com.pnu.sursim.domain.surveyanswer.entity;
+
+import com.pnu.sursim.domain.survey.entity.Question;
+import jakarta.persistence.*;
+import lombok.*;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
+@Getter
+@Builder
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class NumericAnswer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name ="survey_answer_id")
+    private SurveyAnswer surveyAnswer;
+
+
+    @ManyToOne
+    @JoinColumn(name = "question_id")
+    private Question question;
+
+    private int value;
+}
