e09da5d8ce3e6d30955097be2466764fd5961715
JinSeoHyun <15roa1121@gmail.com>
2024-09-24 02:08:39 +0900

    feat: 객관식 응답 엔티티 구현

---
+++ src/main/java/com/pnu/sursim/domain/surveyanswer/entity/MultipleAnswer.java
@@ -0,0 +1,29 @@
+package com.pnu.sursim.domain.surveyanswer.entity;
+
+import com.pnu.sursim.domain.survey.entity.Question;
+import com.pnu.sursim.domain.survey.entity.QuestionOption;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Builder
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class MultipleAnswer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name ="survey_answer_id")
+    private SurveyAnswer surveyAnswer;
+
+    @ManyToOne
+    @JoinColumn(name = "question_id")
+    private Question question;
+
+    @ManyToOne
+    @JoinColumn(name = "option_id")
+    private QuestionOption option;
+}
