abda3754fcd5be4bae125dd8924b0c34c321dd16
JinSeoHyun <15roa1121@gmail.com>
2024-09-24 02:08:03 +0900

    feat: 서술형응답엔티티 구현

---
+++ src/main/java/com/pnu/sursim/domain/surveyanswer/entity/DescriptiveAnswer.java
@@ -0,0 +1,28 @@
+package com.pnu.sursim.domain.surveyanswer.entity;
+
+import com.pnu.sursim.domain.survey.entity.Question;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Builder
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class DescriptiveAnswer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name ="survey_answer_id")
+    private SurveyAnswer surveyAnswer;
+
+    @ManyToOne
+    @JoinColumn(name = "question_id")
+    private Question question;
+
+    @Lob
+    private String text;
+    
+}
