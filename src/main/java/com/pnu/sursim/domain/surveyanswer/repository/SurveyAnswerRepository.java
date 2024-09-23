9c09b37915175139d9c6bf33dc2b35aea566cd64
JinSeoHyun <15roa1121@gmail.com>
2024-09-23 21:27:14 +0900

    chore : SurveyAnswerRepository 리파지토리 위치 변경

--- src/main/java/com/pnu/sursim/domain/surveyanswer/repository/SurveyAnswerRepository.java
+++ src/main/java/com/pnu/sursim/domain/surveyanswer/repository/SurveyAnswerRepository.java
@@ -1,4 +1,4 @@
-package com.pnu.sursim.domain.survey.repository;
+package com.pnu.sursim.domain.surveyanswer.repository;
 
 import com.pnu.sursim.domain.surveyanswer.entity.SurveyAnswer;
 import org.springframework.data.jpa.repository.JpaRepository;
