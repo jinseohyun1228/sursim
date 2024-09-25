a0022eb4235574bc79cd7858948ab7838e50b86f
JinSeoHyun <15roa1121@gmail.com>
2024-09-25 23:12:11 +0900

    feat: 사용자의 응답의문항타입이 적절한지 판단

--- src/main/java/com/pnu/sursim/domain/surveyanswer/dto/AnswerRequest.java
+++ src/main/java/com/pnu/sursim/domain/surveyanswer/dto/AnswerRequest.java
@@ -3,6 +3,7 @@ package com.pnu.sursim.domain.surveyanswer.dto;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.pnu.sursim.domain.survey.entity.Question;
 import com.pnu.sursim.domain.survey.entity.QuestionType;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -14,11 +15,15 @@ import java.util.List;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
-public class QuestionAnswerRequest {
+public class AnswerRequest {
     private Long id;                            //문항 아이디
     private QuestionType questionType;          //문항 타입
     private Integer optionNumber;               //문항 번호
     private String text;                        //문항 텍스트
     private String phoneNumber;                 //전화번호
     private List<Integer> checkList;            //체크리스트
+
+    public boolean validQuestionType(Question question) {
+        return this.questionType == question.getQuestionType();
+    }
 }
