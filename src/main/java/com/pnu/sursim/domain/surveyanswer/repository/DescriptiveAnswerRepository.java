ce921cb0219fff216f8bcce8e24d5028d8f87350
JinSeoHyun <15roa1121@gmail.com>
2024-09-26 02:24:13 +0900

    feat: Descriptive 레포 구현

---
+++ src/main/java/com/pnu/sursim/domain/surveyanswer/repository/DescriptiveAnswerRepository.java
@@ -0,0 +1,8 @@
+package com.pnu.sursim.domain.surveyanswer.repository;
+
+import com.pnu.sursim.domain.surveyanswer.entity.DescriptiveAnswer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DescriptiveAnswerRepository extends JpaRepository<DescriptiveAnswer, Long> {
+
+}
--- src/main/java/com/pnu/sursim/domain/surveyanswer/repository/DescriptiveRepository.java
+++
@@ -1,8 +0,0 @@
-package com.pnu.sursim.domain.surveyanswer.repository;
-
-import com.pnu.sursim.domain.surveyanswer.entity.SurveyAnswer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DescriptiveRepository extends JpaRepository<SurveyAnswer, Long> {
-
-}
