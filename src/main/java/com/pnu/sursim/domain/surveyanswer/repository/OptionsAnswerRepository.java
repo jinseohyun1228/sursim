a3985fe7b374be6c5f78d7de17c385ecd2801f34
JinSeoHyun <15roa1121@gmail.com>
2024-09-26 02:26:01 +0900

    chore: 필요없는 공백 삭제

--- src/main/java/com/pnu/sursim/domain/surveyanswer/repository/OptionsAnswerRepository.java
+++ src/main/java/com/pnu/sursim/domain/surveyanswer/repository/OptionsAnswerRepository.java
@@ -1,6 +1,5 @@
 package com.pnu.sursim.domain.surveyanswer.repository;
 
-import com.pnu.sursim.domain.surveyanswer.entity.DescriptiveAnswer;
 import com.pnu.sursim.domain.surveyanswer.entity.OptionsAnswer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
