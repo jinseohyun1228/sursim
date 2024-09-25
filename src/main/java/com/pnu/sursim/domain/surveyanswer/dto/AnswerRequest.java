517c2c23838bfea0808280ea02054b912929c1a2
JinSeoHyun <15roa1121@gmail.com>
2024-09-26 00:00:52 +0900

    feat: 응답자의 응답에 척도 필드 추가

--- src/main/java/com/pnu/sursim/domain/surveyanswer/dto/AnswerRequest.java
+++ src/main/java/com/pnu/sursim/domain/surveyanswer/dto/AnswerRequest.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.pnu.sursim.domain.survey.entity.Question;
 import com.pnu.sursim.domain.survey.entity.QuestionType;
+import com.pnu.sursim.domain.survey.entity.Scale;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,8 +20,9 @@ public class AnswerRequest {
     private Long id;                            //문항 아이디
     private QuestionType questionType;          //문항 타입
     private Integer optionNumber;               //문항 번호
+    private Scale scale;                      //척도
     private String text;                        //문항 텍스트
-    private String phoneNumber;                 //전화번호
+    private String phoneNumber  ;               //전화번호
     private List<Integer> checkList;            //체크리스트
 
     public boolean validQuestionType(Question question) {
