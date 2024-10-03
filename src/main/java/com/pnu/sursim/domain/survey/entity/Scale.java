13b0539de43a8220ba9e65591ff6b9b8289fde9b
JinSeoHyun <15roa1121@gmail.com>
2024-10-04 04:14:55 +0900

    feat(Scale): 척도의 한글설명 추가

--- src/main/java/com/pnu/sursim/domain/survey/entity/Scale.java
+++ src/main/java/com/pnu/sursim/domain/survey/entity/Scale.java
@@ -1,22 +1,34 @@
 package com.pnu.sursim.domain.survey.entity;
 
+
 import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum Scale {
-    ONE("1"),
-    TWO("2"),
-    THREE("3"),
-    FOUR("4"),
-    FIVE("5");
+    ONE("1", "매우 그렇다."),
+    TWO("2", "그렇다."),
+    THREE("3", "보통이다."),
+    FOUR("4", "그렇지 않다."),
+    FIVE("5", "매우 그렇지 않다.");
 
-    private final String value;
+    private final String value; // DB에 저장할 값
+    private final String description; // 척도의 설명
 
-    Scale(String value) {
+    Scale(String value, String description) {
         this.value = value;
+        this.description = description;
     }
 
     @JsonValue
     public String getValue() {
         return value;
+    }
+
+
+    public int getIntValue() {
+        return Integer.valueOf(value);
+    }
+
+    public String getDescription() {
+        return description;
     }
 }
